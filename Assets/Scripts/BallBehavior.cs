504604949d0493e252872c01700ea52893fb2ef1
Kate Spencer <katemspencer529@gmail.com>
2025-02-06 18:47:10 -0600

    Colliders

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -12,12 +12,16 @@ public class BallBehavior : MonoBehaviour
     Vector2 targetPosition;
     public GameObject target;
     public float minLaunchSpeed;
-    public float maxLaunchSpped;
+    public float maxLaunchSpeed;
     public float minTimeToLaunch;
     public float maxTimeToLaunch;
     public float cooldown;
     public float launchDuration;
     public float timeLastLaunch;
+    Rigidbody2D body;
+    public bool rerouting;
+    public bool launching;
+    public float timeLaunchStart;
     private void Start()
     {
         secondsToMaxSpeed = 30;
@@ -37,39 +41,129 @@ public class BallBehavior : MonoBehaviour
     {
         return Mathf.Clamp01(Time.timeSinceLevelLoad / secondsToMaxSpeed);
     }
-    private void Update()
-    {
-        
-    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+       if(collision.gameObject.tag == "Wall")
+        {
+            targetPosition = getRandomPosition();
+        }
+       if(collision.gameObject.tag == "Ball")
+        {
+            Reroute(collision);
+        }
     }
-    //private void FixedUpdate()
-    //{
-    //    Vector2 currentPosition = gameObject.GetComponent<Transform>().position;
-    //    if (onCooldown() == false)
-    //    {
-    //        if (launching == true)
-    //        {
-    //            float currentLaunchTime = Time.time - timeLaunchStart;
-    //            if (currentLaunchTime > launchDuration)
-    //            {
-    //                startCooldown();
-    //            }
-    //        }
-    //        else
-    //        {
-    //            launch();
-    //        }
-    //    }
-    //}
-    //public void launch()
-    //{
-    //    targetPosition = target.transform.position;
-    //        if (launching == false)
-    //    {
-    //        launching = true;
-    //    }
-    //}
+    public void initialPosition()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.position = getRandomPosition();
+        targetPosition = getRandomPosition();
+        launching = false;
+        rerouting = true;
+
+    }
+    public void Reroute(Collision2D collision)
+    {
+        GameObject otherBall = collision.gameObject;
+        if(rerouting == true)
+        {
+            otherBall.GetComponent<BallBehavior>().rerouting = false;
+            Rigidbody2D ballBody = otherBall.GetComponent<Rigidbody2D>();
+            Vector2 contact = collision.GetContact(0).normal;
+            targetPosition = Vector2.Reflect(targetPosition, contact).normalized;
+            launching = false;
+            float separationDistance = 0.1f;
+            ballBody.position += contact * separationDistance;
+        }
+        else
+        {
+            rerouting = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        body = GetComponent<Rigidbody2D>();
+        Vector2 currentPosition = body.position;
+        if (onCooldown() == false)
+        {
+            if (launching == true)
+            {
+                float currentLaunchTime = Time.time - timeLaunchStart;
+                if (currentLaunchTime > launchDuration)
+                {
+                    startCooldown();
+                }
+            }
+            else
+            {
+                launch();
+            }
+            Vector2 currentPos = gameObject.GetComponent<Transform>().position;
+            float distance = Vector2.Distance(currentPos, targetPosition);
+            if (distance > 0.1)
+            {
+                float difficulty = getDifficultyPercentage();
+                float currentSpeed;
+                if (launching == true)
+                {
+                    float launchingForHowLong = Time.time - timeLaunchStart;
+                    if (launchingForHowLong > launchDuration)
+                    {
+                        startCooldown();
+                    }
+                    currentSpeed = Mathf.Lerp(minLaunchSpeed, maxLaunchSpeed,
+                    difficulty);
+                }
+                else
+                {
+                    currentSpeed = Mathf.Lerp(minSpeed, maxSpeed, difficulty);
+                }
+                currentSpeed = currentSpeed * Time.deltaTime;
+                Vector2 newPosition = Vector2.MoveTowards(currentPos, targetPosition,
+                currentSpeed);
+                transform.position = newPosition;
+                body.MovePosition(newPosition);
+
+            }
+            else
+            { // You are at target
+                if (launching == true)
+                {
+                    startCooldown();
+                }
+                targetPosition = getRandomPosition();
+            }
+        }
+    }
+    public float getDifficultyPercentage()
+    {
+        float difficulty = Mathf.Clamp01(Time.timeSinceLevelLoad /
+        secondsToMaxSpeed);
+        return difficulty;
+    }
+
+    public void launch()
+    {
+        Rigidbody2D targetBody = target.GetComponent<Rigidbody2D>();
+        targetPosition = targetBody.position;
+        if (launching == false)
+        {
+            launching = true;
+        }
+    }
+    public bool onCooldown()
+    {
+        bool result = false;
+        float timeSinceLastLaunch = Time.time - timeLastLaunch;
+        if (timeSinceLastLaunch < cooldown)
+        {
+            result = true;
+        }
+        return result;
+    }
+    public void startCooldown()
+    {
+        timeLastLaunch = Time.time;
+        launching = false;
+    }
 }
