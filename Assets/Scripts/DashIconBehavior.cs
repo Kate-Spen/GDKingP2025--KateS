32e69d5a35b3b5a9b95ad9ee98004d721402ee5c
Kate Spencer <katemspencer529@gmail.com>
2025-02-12 15:48:56 -0600

    Dash and Dashicon
    Implemented dash and dash icon as well as the animation.

---
+++ Assets/Scripts/DashIconBehavior.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DashIconBehavior : MonoBehaviour
+{
+    TextMeshProUGUI label;
+    Image overlay;
+    float cooldown;
+    float cooldownRate;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        label = GetComponentInChildren<TextMeshProUGUI>();
+        Image[] images = GetComponentsInChildren<Image>();
+        cooldownRate = PinBehavior.cooldownRate;
+        for (int i = 0; i < images.Length; i++)
+        {
+            if (images[i].tag == "overlay")
+            {
+                overlay = images[i];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown = PinBehavior.cooldown;
+        string message = "";
+        if (cooldown > 0.0)
+        {
+            float fill = cooldown / cooldownRate;
+            message = string.Format("{0:0.0}", cooldown);
+            overlay.fillAmount = fill;
+        }
+        label.SetText(message);
+    }
+}
--- Assets/Scripts/PinBehavior.cs
+++ Assets/Scripts/PinBehavior.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class PinBehavior : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float speed;
+    public float baseSpeed = 2.0f;
+    public float dashSpeed = 5.0f;
+    public bool dashing;
     public Vector2 newPosition;
     public Vector3 mousePosG;
+    public float dashDuration;
+    public float start;
+    //variables for dash cooldown
+    public static float cooldownRate = 1.0f;
+    public static float cooldown = 0.0f;
+    public float timelastDashEnded;
+
     Camera cam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
@@ -16,7 +26,7 @@ public class PinBehavior : MonoBehaviour
         mousePosG = cam.ScreenToWorldPoint(Input.mousePosition);
         newPosition = Vector2.MoveTowards(transform.position, mousePosG, speed* Time.fixedDeltaTime);
         transform.position = newPosition;
-
+        Dash();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -25,6 +35,34 @@ public class PinBehavior : MonoBehaviour
         if (collided == "Ball" || collided == "Wall")
         {
             Debug.Log("Game Over");
+        }
+    }
+    private void Dash()
+    {
+        if (dashing == true)
+        {
+            float currenttime = Time.time;
+            float timeDashing = currenttime - start;
+            if (timeDashing > dashDuration)
+            {
+                dashing = false;
+                speed = baseSpeed;
+                cooldown = cooldownRate;
+            }
+        }
+        else
+        {
+            cooldown = cooldown - Time.deltaTime;
+            if(cooldown < 0.0)
+            {
+                cooldown = 0.0f;
+            }
+            if (Input.GetMouseButtonDown(0) == true && cooldown == 0.0)
+            {
+                dashing = true;
+                speed = dashSpeed;
+                start = Time.time;
+            }
         }
     }
 }
