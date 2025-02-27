4c192b7ab91befad1056c2b66eb267970157a063
Kate Spencer <katemspencer529@gmail.com>
2025-02-26 21:23:25 -0600

    added explosion and pickup FX

---
+++ Assets/Scripts/ScriptableObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewUser", menuName = "ScriptableObjects/Users", order = 1)]
+public class User : ScriptableObject
+{
+    public string pName;
+    public int health;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
